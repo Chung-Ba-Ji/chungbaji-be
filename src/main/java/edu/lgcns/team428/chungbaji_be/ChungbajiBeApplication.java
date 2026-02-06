e77d9295c06744548d1fb18d62b6fb83b972ed9c
JunYoung Lee <oonju5031@gmail.com>
2026-02-06 14:22:29 +0900

    chore: 프로젝트 기본 구조 생성

---
+++ src/main/java/edu/lgcns/team428/chungbaji_be/ChungbajiBeApplication.java
@@ -0,0 +1,13 @@
+package edu.lgcns.team428.chungbaji_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChungbajiBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChungbajiBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/lgcns/team428/chungbaji_be/ChungbajiBeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.lgcns.team428.chungbaji_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChungbajiBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
