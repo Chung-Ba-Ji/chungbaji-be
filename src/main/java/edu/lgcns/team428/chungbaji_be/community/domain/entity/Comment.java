e35fe3f7ccfadac00af5e4e8514c422e426dbf6c
kimhyeonsu23 <phantom9507@naver.com>
2026-02-09 17:01:45 +0900

    comment entity 작성

--- src/main/java/edu/lgcns/team428/chungbaji_be/community/domain/entity/Comment.java
+++ src/main/java/edu/lgcns/team428/chungbaji_be/community/domain/entity/Comment.java
@@ -1,4 +1,41 @@
 package edu.lgcns.team428.chungbaji_be.community.domain.entity;
 
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comment")
+@EntityListeners(AuditingEntityListener.class)
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer commentId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = false, length = 20)
+    private String status = "CREATED";
+    
 }
