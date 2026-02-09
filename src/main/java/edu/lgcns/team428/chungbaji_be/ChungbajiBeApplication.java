dc6d69fe1868c02e533592ef8340ab97cc7818ab
JunYoung Lee <oonju5031@users.noreply.github.com>
2026-02-09 10:04:09 +0900

    chore: 기초 환경설정 및 환경변수 gitignore 처리

--- src/main/java/edu/lgcns/team428/chungbaji_be/ChungbajiBeApplication.java
+++ src/main/java/edu/lgcns/team428/chungbaji_be/ChungbajiBeApplication.java
@@ -1,12 +1,19 @@
 package edu.lgcns.team428.chungbaji_be;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChungbajiBeApplication {
-
 	public static void main(String[] args) {
+
+		Dotenv env = Dotenv.configure().ignoreIfMissing().load();
+
+		env.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
+
 		SpringApplication.run(ChungbajiBeApplication.class, args);
 	}
 
