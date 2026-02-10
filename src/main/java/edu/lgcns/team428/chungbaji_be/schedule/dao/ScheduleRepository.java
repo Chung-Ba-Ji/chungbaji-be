516fc178ef14419b965b0d313f52806dca47283d
Yeongeunn <nye0817@gmail.com>
2026-02-10 16:26:55 +0900

    feat: 스케줄러 Repository 추가 및 개발 환경 설정 업데이트
    - 자동화 처리를 위한 ScheduleRepository 구현\n- Security 및 JPA 로깅 설정을 application-dev.yml에 반영

--- src/main/java/edu/lgcns/team428/chungbaji_be/schedule/dao/ScheduleRepository.java
+++ src/main/java/edu/lgcns/team428/chungbaji_be/schedule/dao/ScheduleRepository.java
@@ -1,5 +1,9 @@
 package edu.lgcns.team428.chungbaji_be.schedule.dao;
 
-public interface ScheduleRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import edu.lgcns.team428.chungbaji_be.schedule.domain.entity.ScheduleEntity;
+
+@Repository
+public interface ScheduleRepository extends JpaRepository<ScheduleEntity, Integer> {
 }
